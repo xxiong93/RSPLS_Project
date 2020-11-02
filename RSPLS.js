6ce5864b545b623728a5f70e388477756a35b07c
xxiong93 <xengxiong93@gmail.com>
2020-11-01 22:00:12 -0600

    Added in method calls for the players and AI to choose a gesture.  Also switched the location of creation for choosing a gesture.

--- RSPLS.js
+++ RSPLS.js
@@ -1,108 +1,108 @@
 "use strict";
 class Game{
     constructor(){
-        this.playerOne = new Player();
-        this.playerTwo = new Player();
-        this.playerAi = new Machine();
-
-        this.gesture = ["rock", "paper", "scissors", "lizard", "spock"];;
+        this.playerOne = null;
+        this.playerTwo = null;
+        //game setup to create human or ai
+        
+        //this.gesture = ["rock", "paper", "scissors", "lizard", "spock"];
     }
     playGame(){
         //display human v human or human v AI option
-        let ask = prompt("Would you like to play against another person or the program? Enter 'human' or 'machine'");
-        if(ask === 'human'){
+        let choice = prompt("Would you like to play against another person or the program? Enter 'human' or 'machine'");
+        let playerGesture;
+        if(choice === 'human'){
             //run human v human 
-    
+            this.playerOne = new Human("Steve Rogers");
+            this.playerTwo = new Human("Sam Wilson");
+            playerGesture = playerOne.chooseGesture(this.gesture);
+            playerGesture = playerTwo.chooseGesture(this.gesture);
+
+
         }
-        else if(ask === 'machine'){
+        else if(choice === 'machine'){
             //run machine v machine
+            this.playerOne = new Human("Tony Stark");
+            this.playerTwo = new Machine("Ultron");
+            playerGesture = playerOne.chooseGesture(this.gesture);
+            playerGesture = Machine.randomGesture(this.gesture);
         }
+
+        
         //must run the game at least 3 times
             //keep track of points after each "game"
             //update point total and then re-run function
         //end game after 3 games
     
-    
+        
+        gestureWinCondition(gestureOne, gestureTwo)
+            if(gestureOne === 'rock' && gestureTwo === 'scissors' || gestureOne === 'rock' && gestureTwo === 'lizard'){
+                console.log("You win!");
+            }
+            else if(gestureOne === 'scissors' && gestureTwo === 'paper' || gestureOne === 'scissors' && gestureTwo === 'lizard'){
+                console.log("You win!");
+            }
+            else if(gestureOne === 'paper' && gestureTwo === 'rock' || gestureOne === 'paper' && gestureTwo === 'spock'){
+                console.log("You win!");
+            }
+            else if(gestureOne === 'lizard' && gestureTwo === 'spock' || gestureOne === 'lizard' && gestureTwo === 'paper'){
+                console.log("You win!");
+            }
+            else if(gestureOne === 'spock' && gestureTwo === 'rock' || gestureOne === 'spock' && gestureTwo === 'scissors'){
+                console.log("You win!");
+            }
+            else{
+                console.log("You lose. Better luck next time!");
+            }
+        
+        }
     }
-    
-    
-
 }
 class Player{
     constructor(name){
         this.name = name;
         this.score = 0;
+        this.gesture = ["rock", "paper", "scissors", "lizard", "spock"];
         
     }
-    
+    chooseGesture(gesture){
+ 
+        let choice = prompt("What gesture do you choose? 1 = rock, 2 = paper, 3 = scissors, 4 = lizard, and 5 = spock");//could set gestures to numerical values for easier use
+        //this is to access gesture from Player
+            return choice;
+            }
+    randomGesture(gesture){
+        
+        let gestureFound;
+            for(let i = Math.floor(Math.random()* 5);i < gesture.length; i++){
+                gestureFound = gesture[i];
+                return gestureFound;
+            }
+                
+        }
 }
+
+
 
 class Human extends Player{
     constructor(name){
         super(name);
     }
-    //chooseGesture(gesture);
-    //add in a prompt to choose gesture that will be used
-    //validate entry as an available gesture
-    //
-}
+
 
 class Machine extends Player{
     constructor(name){
         super(name);
     }
-    //randomGesture(gesture);
-}
-
-let player1 = new Human("Steve Irwin");
-let player2 = new Human("Jeff Corwin");
-let aiPlayer = new Machine("Robot");
-
-
-function randomGesture(gesture){
-
-    for(let i = Math.floor(Math.random()* 5);i < gesture.length; i++){
-        console.log(gesture[i]);
-        return;
-    }
     
 }
 
-function chooseGesture(gesture){
- 
-    let ask = prompt("What gesture do you choose? 'rock', 'paper', 'scissors', 'lizard', or 'spock'");
-    let choice = "";
-    if(ask === 'rock' || ask === 'paper' || ask === 'scissors' || ask === 'lizard' || ask === 'spock'){
-        choice = ask;
-        return choice;
-    }
-    else{
-        chooseGesture(choice);
-        return; //validate gesture choice and prompt again
-    }
+
+game.playGame();
+
+
+
     
-}
-chooseGesture();
-function gestureWinCondition(gestureOne, gestureTwo){
-    if(gestureOne === 'rock' && gestureTwo === 'scissors' || gestureOne === 'rock' && gestureTwo === 'lizard'){
-        console.log("You win!");
-    }
-    else if(gestureOne === 'scissors' && gestureTwo === 'paper' || gestureOne === 'scissors' && gestureTwo === 'lizard'){
-        console.log("You win!");
-    }
-    else if(gestureOne === 'paper' && gestureTwo === 'rock' || gestureOne === 'paper' && gestureTwo === 'spock'){
-        console.log("You win!");
-    }
-    else if(gestureOne === 'lizard' && gestureTwo === 'spock' || gestureOne === 'lizard' && gestureTwo === 'paper'){
-        console.log("You win!");
-    }
-    else if(gestureOne === 'spock' && gestureTwo === 'rock' || gestureOne === 'spock' && gestureTwo === 'scissors'){
-        console.log("You win!");
-    }
-    else{
-        console.log("You lose. Better luck next time!");
-    }
 
 
 
-}
